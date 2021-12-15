10a03371808778e310b30f1b390835e3be2d74ce
aleex16sanchez <asanchezrodriguez.sanjose@alumnado.fundacionloyola.net>
2021-12-15 11:46:52 +0100

    v3

--- Integrado/src/paquete/ClienteFTP.java
+++ Integrado/src/paquete/ClienteFTP.java
@@ -1,6 +1,9 @@
 package paquete;
 
 import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -12,7 +15,6 @@ public class ClienteFTP {
 		String ftp = "127.0.0.1";
 		String user = "EmpresarioPrueba";
 		String password = "empresario";
-		
 		try {
 			cliente.connect(ftp);
 			boolean login = cliente.login(user, password);
@@ -24,13 +26,31 @@ public class ClienteFTP {
 			String archivo = "miarchivo.txt";
 			FileInputStream fileIn = new FileInputStream(rutaArchivo);
 			cliente.changeWorkingDirectory(rutaRemota);
-			boolean uploadFile = cliente.storeFile(archivo,fileIn);
+			boolean uploadFile = cliente.storeFile(archivo, fileIn);
 			if (!uploadFile) {
 				throw new Exception("Error al subir el archivo.");
 			}
 			cliente.logout();
 			cliente.disconnect();
 		} catch (Exception e) {
+		}
+	}
+
+	public void conexion() {
+		Connection conexion;
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String ip = "jdbc:mysql://db4free.net:3306/grupo3integrado";
+		String usuario = "integradogrupo3";
+		String password = "#Integradogrupo3";
+		try {
+			Class.forName(driver);
+			conexion = DriverManager.getConnection(ip, usuario, password);
+			System.out.println("Escribe tu nombre para el login");
+//			String sql = "SELECT NOMBRE, PASSWORD FROM `USUARIOS` WHERE NOMBRE, PASSWORD LIKE " + ; 
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 
